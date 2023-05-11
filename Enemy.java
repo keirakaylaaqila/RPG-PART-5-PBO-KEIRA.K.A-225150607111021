15a7adeae372309b8ba0cefe8f808d17beab5593
keirakaylaaqila <133154668+keirakaylaaqila@users.noreply.github.com>
2023-05-11 13:15:48 +0700

    Add files via upload

---
+++ Enemy.java
@@ -0,0 +1,11 @@
+public class Enemy extends Character {
+@Override
+    
+ public void move() {
+        System.out.println("MoveEnemy!!");
+    }
+public void move(int action) {
+        System.out.println("MoveEnemy!!" + action+ " Action!!");
+    }
+
+}
